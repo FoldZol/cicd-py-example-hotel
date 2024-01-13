313252b0907b12ed3b194572525e7fc7b3f502e5
FoldZol <142608732+FoldZol@users.noreply.github.com>
2024-01-13 16:44:08 +0100

    Update test_hootel.py

--- test/test_hootel.py
+++ test/test_hootel.py
@@ -2,8 +2,11 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 import allure
 import pytest
+import random
 
 
 class TestHootel(object):
@@ -19,13 +22,17 @@ class TestHootel(object):
     def teardown_method(self):
         self.browser.quit()
 
-    @pytest.mark.parametrize("email, password", [('hiwasi1765@wisnick.com', 'tesztelek2021'), ('', '')])
+    @pytest.mark.parametrize("email, password", [('progmtest4@proton.me', 'ProgMtest4')])
     @allure.title("Hootel Login")
     @allure.description("A belépés tesztelése")
     @allure.severity(allure.severity_level.TRIVIAL)
     @allure.tag("login")
     def test_login(self, email, password):
-        login_btn = self.browser.find_element(By.XPATH, '//a[@class="nav-link"]')
+        menu_toggle = WebDriverWait(self.browser, 5).until(
+            ec.element_to_be_clickable((By.XPATH, "//button[@class = 'navbar-toggler collapsed']")))
+        menu_toggle.click()
+        login_btn = WebDriverWait(self.browser, 5).until(
+            ec.element_to_be_clickable((By.XPATH, '//a[@class="nav-link"]')))
         login_btn.click()
 
         email_input = self.browser.find_element(By.ID, 'email')
@@ -36,9 +43,8 @@ class TestHootel(object):
 
         submit_btn = self.browser.find_element(By.NAME, 'submit')
         submit_btn.click()
-        time.sleep(1)
 
-        logout_btn = self.browser.find_element(By.ID, 'logout-link')
+        logout_btn = WebDriverWait(self.browser, 5).until(ec.element_to_be_clickable((By.ID, "logout-link")))
 
         assert logout_btn.text == "Kilépés"
 
@@ -50,3 +56,90 @@ class TestHootel(object):
         hotel_list = self.browser.find_elements(By.XPATH, '//h4[@style="cursor: pointer"]')
         assert len(hotel_list) != 0
         assert len(hotel_list) == 10
+
+    def test_hotel_description_rule(self):
+        # random hotel kiválasztása és adatlapjának megnyitása
+        hotel_list_btn = self.browser.find_element(By.XPATH, "//button[text()=' Megnézem a teljes listát ']")
+        hotel_list_btn.click()
+        hotels = WebDriverWait(self.browser, 5).until(
+            ec.presence_of_all_elements_located((By.XPATH, "//button[text() = 'Megnézem']")))
+        random.choice(hotels).click()
+
+        # description begyűjtése
+        # time.sleep nélkül 2 elemet gyűjtött be a WebDriverWait mivel, amit talált az igazzá tette a presence of all elements-et. Emiatt force-ni kell a time.sleep-et.
+        time.sleep(1)
+        description = WebDriverWait(self.browser, 5).until(
+            ec.presence_of_all_elements_located((By.XPATH, f"//p[@class='card-text']")))[1].text
+
+        assert len(description) >= 500
+
+    def test_logout(self):
+        # bejelentkezés
+        menu_toggle = WebDriverWait(self.browser, 5).until(
+            ec.element_to_be_clickable((By.XPATH, "//button[@class = 'navbar-toggler collapsed']")))
+        menu_toggle.click()
+        login_button = WebDriverWait(self.browser, 5).until(
+            ec.element_to_be_clickable((By.XPATH, '//a[@class="nav-link"]')))
+        login_button.click()
+
+        email_input = self.browser.find_element(By.ID, 'email')
+        email_input.send_keys('progmtest4@proton.me')
+
+        password_input = self.browser.find_element(By.ID, 'password')
+        password_input.send_keys('ProgMtest4')
+
+        submit_button = self.browser.find_element(By.NAME, 'submit')
+        submit_button.click()
+
+        # kijelentkezés
+        logout_button = WebDriverWait(self.browser, 5).until(ec.element_to_be_clickable((By.ID, "logout-link")))
+        logout_button.click()
+
+        # sikeres kijelentkezés ellenőrzése
+        login_button = WebDriverWait(self.browser, 5).until(
+            ec.element_to_be_clickable((By.XPATH, '//a[@class="nav-link"]')))
+
+        assert login_button.text == "Bejelentkezés"
+
+    def test_user_booking_button(self):
+        menu_toggle = WebDriverWait(self.browser, 5).until(
+            ec.element_to_be_clickable((By.XPATH, "//button[@class = 'navbar-toggler collapsed']")))
+        menu_toggle.click()
+        login_button = WebDriverWait(self.browser, 5).until(
+            ec.element_to_be_clickable((By.XPATH, '//a[@class="nav-link"]')))
+        login_button.click()
+
+        email_input = self.browser.find_element(By.ID, 'email')
+        email_input.send_keys('progmtest4@proton.me')
+
+        password_input = self.browser.find_element(By.ID, 'password')
+        password_input.send_keys('ProgMtest4')
+
+        submit_button = self.browser.find_element(By.NAME, 'submit')
+        submit_button.click()
+        bookings_btn = WebDriverWait(self.browser, 5).until(
+            ec.element_to_be_clickable((By.XPATH, "//a[@id = 'user-bookings']")))
+        bookings_btn.click()
+
+        menu_toggle = WebDriverWait(self.browser, 5).until(
+            ec.element_to_be_clickable((By.XPATH, "//button[@class = 'navbar-toggler']")))
+        menu_toggle.click()
+
+        assert self.browser.current_url == "http://hotel-v3.progmasters.hu/bookings"
+
+    def test_delete_booking(self):
+        menu_toggle = WebDriverWait(self.browser, 5).until(
+            ec.element_to_be_clickable((By.XPATH, "//button[@class = 'navbar-toggler collapsed']")))
+        menu_toggle.click()
+        login_button = WebDriverWait(self.browser, 5).until(
+            ec.element_to_be_clickable((By.XPATH, '//a[@class="nav-link"]')))
+        login_button.click()
+
+        email_input = self.browser.find_element(By.ID, 'email')
+        email_input.send_keys('progmtest4@proton.me')
+
+        password_input = self.browser.find_element(By.ID, 'password')
+        password_input.send_keys('ProgMtest4')
+
+        submit_button = self.browser.find_element(By.NAME, 'submit')
+        submit_button.click()
